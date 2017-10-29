83680bbaa407d3a3e7a598a75a4ab84ea2c62e79
dgtlslave <slavko.alexandr@gmail.com>
2017-10-29 21:03:55 +0200

    add the dream team memder

---
+++ src/js/script.js
@@ -0,0 +1,47 @@
+//=require jquery.min.js
+
+// to inlude plugins add "=" sign below
+// require bootstrap.min.js
+// require slick.min.js 
+
+// require popper-utils.min.js
+// require popper.min.js
+
+//=require util.js
+//=require tab.js
+
+//=require isotope.pkgd.min.js
+
+;(function($){
+
+	$(document).ready(function(){
+		var $grid;
+
+		$grid = $('.ak-portfolio__items').isotope({
+		  itemSelector: '.ak-portfolio__item',
+		  percentPosition: true,
+		  masonry: {
+		    // use outer width of grid-sizer for columnWidth
+		    columnWidth: '.ak-portfolio__sizer'
+		  }
+		});
+
+		$('.ak-portfolio__filter').on('click', 'a', function(evt){
+			evt.preventDefault();
+
+			var filterValue = $(this).attr('data-filter');
+  			$grid.isotope({ filter: filterValue });
+
+  			$('.ak-portfolio__filter a').removeClass('active');
+  			$(this).addClass('active');
+		});
+
+		$('#mobile-menu-link').on('click', function(evt) {
+			evt.preventDefault();
+			$(this).toggleClass('open');
+			$('#mobile-menu').toggleClass('ak-menu--open');
+		});
+
+	});
+
+})(jQuery);
